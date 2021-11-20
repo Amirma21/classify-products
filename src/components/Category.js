450e8bf7c74243cb8b02c55975d5553328c2a6d3
amir maleki <amirmaleki21physics@gmail.com>
2021-11-20 16:13:00 +0330

    style of input

--- src/components/Category.js
+++ src/components/Category.js
@@ -1,11 +1,14 @@
 import React from "react";
+import "./category.css"
 
 const Group = ({ change, inputValue , submit }) => {
   return (
     <div>
-      <p>Hint : you can add your new group , if not just existed</p>
-      <form>
+      <p className="hint">Hint : you can add your new group , if not just existed</p>
+     <div className="formContainer">
+     <form>
         <input
+        className="input"
           type="text"
           value={inputValue}
           onChange={(event) => {
@@ -13,13 +16,15 @@ const Group = ({ change, inputValue , submit }) => {
           }}
         ></input>
         <input
+        className="btn"
           type="submit"
-          value="add"
+          value="+"
           onClick={(event) => {
             submit(event);
           }}
         ></input>
       </form>
+     </div>
     </div>
   );
 };
